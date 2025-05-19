e039f601e82a85e27f24c89d07d6c8999979df6c
Tejasvi Garg <gargtejasvi076@gmail.com>
2025-05-19 23:16:28 +0530

    n

--- ReverseString.java
+++ ReverseString.java
@@ -1,10 +1,27 @@
-public class ReverseString{
-    public static void main (String[] args){
-        String input ="Hello World";
-        String reversed =" ";
+// public class ReverseString{
+//     public static void main (String[] args){
+//         String input ="Hello World";
+//         String reversed =" ";
 
-        for (int i=input.length()-1; i>=0; i--){
-            reversed = reversed + input.charAt(i);      //reverse without using built-in function
+//         for (int i=input.length()-1; i>=0; i--){
+//             reversed = reversed + input.charAt(i);      //reverse without using built-in function
+//         }
+//         System.out.println(reversed);
+//     }
+// }
+
+import java.util.Scanner;
+
+public class ReverseString{  
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the String:");
+        String input = sc.next();
+        String reversed = " ";
+
+        for (int i=input.length()-1; i>=0;  i--){
+            reversed = reversed+input.charAt(i);
         }
         System.out.println(reversed);
     }
