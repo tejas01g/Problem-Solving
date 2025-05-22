fe4752081d9c2e08905806ed2dece245b5629302
Tejasvi Garg <gargtejasvi076@gmail.com>
2025-05-22 11:48:16 +0530

    change

--- fibonacci.java
+++ fibonacci.java
@@ -1,10 +1,32 @@
+// public class fibonacci {
+//     public static void main(String[] args) {
+//         int a = 0 , b=1 , n= 10;
+
+//         for (int i=2; i<=n; i++){     
+//             int next = a+b;
+//             System.out.println(next);
+//             a=b;
+//             b=next;
+//         }
+        
+//     }
+// }
+
+import java.util.Scanner;
+
 public class fibonacci {
     public static void main(String[] args) {
-        int a = 0 , b=1 , n= 10;
+        Scanner sc = new Scanner(System.in);
+        int a = 0 , b=1;
+
+        System.out.println("The Fibonacci series" + a + " ," +b);
+
+        System.out.println("Enter the value of n:");
+        int n = sc.nextInt();
 
         for (int i=2; i<=n; i++){
             int next = a+b;
-            System.out.println(next);
+            System.out.println(" " + next);
             a=b;
             b=next;
         }
