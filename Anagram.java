c92e29a82102c15ab82a76534b31c56b3d61de77
tejas01g <gargtejasvi076@gmail.com>
2025-08-02 10:34:08 +0530

    n

--- Anagram.java
+++ Anagram.java
@@ -1,54 +1,27 @@
-// import java.util.Arrays;
-
-// public class Anagram {
-//     public static void main(String[] args) {
-//         String s1 = "SILENT";
-//         String s2 = "LISTEN";
-
-//         char[] c1 = s1.toCharArray();
-//         char[] c2 = s2.toCharArray();
-
-//         if (c1.length != c2.length){
-//             System.out.println("This is not Anagram");
-//             System.exit(0);
-//         } 
-//         Arrays.sort(c1);
-//         Arrays.sort(c2);
-
-//         for (int i = 0; i<c1.length; i++){
-//             if (c1[i] != c2[i]){
-//                 System.out.println("This is not Anagram");
-//                 System.exit(0);
-//             }
-            
-//         }
-//         System.out.println("This is Anagram");
-//     }
-// }
-
 import java.util.Arrays;
 
-public class Anagram {
+public class Anagram{
     public static void main(String[] args) {
+        
         String s1 = "LISTEN";
-        String s2 = "SILNT";
+        String s2 = "SILENT";
 
         char[] c1 = s1.toCharArray();
         char[] c2 = s2.toCharArray();
 
         if(c1.length != c2.length){
-            System.out.println("This is not Anagram");
-            System.exit(0);
+            System.out.println("false");
         }
+
         Arrays.sort(c1);
         Arrays.sort(c2);
 
         for(int i = 0; i<c1.length; i++){
             if(c1[i] != c2[i]){
-                System.out.println("THis is not Anagram");
+                System.out.println("false");
             }
         }
-        System.out.println("THis is Anagram");
-
+        System.out.println("true");
+        
     }
 }
